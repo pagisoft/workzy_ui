aa0c7098f4f0a71ac3f56c8a52b1993c3183f926
Grzegorz Macialek <grzegorzmacialek@gmail.com>
2019-11-29 19:50:25 +0100

    Added apiUrl

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -5,6 +5,7 @@ import { of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { HttpClient ,HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: "root"
@@ -13,8 +14,8 @@ export class AuthService {
   //Only for demo purpose
   authenticated = true;
 
-  constructor(private store: LocalStoreService, 
-    public http: HttpClient ,private router: Router) {
+  constructor(private store: LocalStoreService,
+    public http: HttpClient, private router: Router) {
     this.checkAuth();
   }
 
@@ -28,20 +29,20 @@ export class AuthService {
 
   signupTalent(creds) {
 
-   
+
     let body = JSON.stringify(creds);
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
 
-     return this.http.post<any>('http://192.168.1.31:8090/signup',body,{headers: headers})
+     return this.http.post<any>(environment.apiUrl + 'signup', body,{headers: headers})
          .map(response => {
-            
+
             of({}).pipe(delay(1500));
             return response;
           })
-         
 
-   
+
+
   }
 
   signin(credentials) {
@@ -50,27 +51,27 @@ export class AuthService {
 
             "email":credentials.email,
             "password":credentials.password
-        
+
     }
-   
+
     let body = JSON.stringify(creds);
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
 
-     return this.http.post<any>('http://192.168.1.31:8090/auth/signin',body,{headers: headers})
+     return this.http.post<any>(environment.apiUrl + 'auth/signin', body,{headers: headers})
          .map(response => {
-                  
+
             this.authenticated = true;
-            this.store.setItem('ut', response.token); 
-            
-            this.store.setItem('iamvt', true); 
-           
+            this.store.setItem('ut', response.token);
+
+            this.store.setItem('iamvt', true);
+
             of({}).pipe(delay(1500));
             return response;
           })
-         
 
-   
+
+
   }
   signout() {
     this.authenticated = false;
